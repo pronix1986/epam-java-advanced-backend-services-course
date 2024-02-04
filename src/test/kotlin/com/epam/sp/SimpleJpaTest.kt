85978f916302eb6947ec7c8858e01a1786da2a42
Siarhei_Prakharenka <siarhei_prakharenka@epam.com>
2024-02-04 20:23:11 +0300

    Task 3,4 - Spring Profiles, Spring Actuator

--- src/test/kotlin/com/epam/sp/SimpleJpaTest.kt
+++ src/test/kotlin/com/epam/sp/SimpleJpaTest.kt
@@ -4,7 +4,7 @@ import com.epam.sp.jpa.entities.Order
 import com.epam.sp.jpa.repositories.OrderRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.test.context.ActiveProfiles
 import kotlin.test.Test
@@ -12,7 +12,9 @@ import kotlin.test.assertEquals
 
 // Task 3.4. Adjust test from the Task 1 to use Data Source
 // configured from test configuration file(@DataJpaTest/@DataJdbcTest + @AutoConfigureTestDatabase(replace = NONE))
-@DataJpaTest
+
+//@DataJpaTest - the annotation was replaced with @SpringBootTest, otherwise it doesn't work well (if at all) with WebMvc
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @ComponentScan("com.epam.sp")
