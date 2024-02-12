2385fcc2e224f12fa47b42a2b6955fa62a7c2b6e
Siarhei_Prakharenka <siarhei_prakharenka@epam.com>
2024-02-12 22:29:40 +0300

    Move Task 1 to a dedicated module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.serialization")
-    kotlin("plugin.spring")
-    kotlin("plugin.jpa")
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    val kotlinVersion: String by project
-
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    // Task 4.1. Enable actuator by adding https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-actuator dependency.
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-hateoas")
-
-
-    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    runtimeOnly("com.h2database:h2")
-
-    testImplementation(kotlin("test"))
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "21"
-        freeCompilerArgs = listOf("-Xjsr305=strict") // `-Xjsr305=strict` enables the strict mode for JSR-305 annotations
-    }
-}
-
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
-    archiveFileName = "app.jar"
+    kotlin("jvm") apply false
+    kotlin("plugin.serialization") apply false
+    kotlin("plugin.spring") apply false
+    kotlin("plugin.jpa") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management") apply false
 }
